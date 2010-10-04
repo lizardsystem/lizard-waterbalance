ee50443c2a0d7c3ef7f94ff586e03254ab825461
reinout <reinout@vanrees.org>
2010-10-04 13:34:21 +0000

    Generated with nensskel

---
+++ lizard_waterbalance/testsettings.py
@@ -0,0 +1,39 @@
+DEBUG = True
+TEMPLATE_DEBUG = True
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = 'test.db'
+SITE_ID = 1
+INSTALLED_APPS = [
+    'lizard_waterbalance',
+    'lizard_ui',
+    'staticfiles',
+    'compressor',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    ]
+ROOT_URLCONF = 'lizard_waterbalance.urls'
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+# Used for django-staticfiles
+STATIC_URL = '/static_media/'
+TEMPLATE_CONTEXT_PROCESSORS = (
+    # Default items.
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    # Needs to be added for django-staticfiles to allow you to use
+    # {{ STATIC_URL }}myapp/my.css in your templates.
+    'staticfiles.context_processors.static_url',
+    )
+
+
+try:
+    # Import local settings that aren't stored in svn.
+    from lizard_waterbalance.local_testsettings import *
+except ImportError:
+    pass
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+version = '0.1dev'
+
+long_description = '\n\n'.join([
+    open('README.txt').read(),
+    open('TODO.txt').read(),
+    open('CREDITS.txt').read(),
+    open('CHANGES.txt').read(),
+    ])
+
+install_requires = [
+    'Django',
+    'django-staticfiles',
+    'lizard-ui',
+    'django-nose',
+    'nosexcover',
+    ],
+
+tests_require = [
+    ]
+
+setup(name='lizard-waterbalance',
+      version=version,
+      description="TODO",
+      long_description=long_description,
+      # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=['Programming Language :: Python',
+                   'Framework :: Django',
+                   ],
+      keywords=[],
+      author='TODO',
+      author_email='TODO@nelen-schuurmans.nl',
+      url='',
+      license='GPL',
+      packages=['lizard_waterbalance'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      tests_require=tests_require,
+      extras_require = {'test': tests_require},
+      entry_points={
+          'console_scripts': [
+          ]},
+      )
