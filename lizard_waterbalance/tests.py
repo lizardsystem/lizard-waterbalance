e98396c9b7d2033615540c6c56257fcc8a3f49f8
swinkels <swinkels.pieter@yahoo.com>
2010-11-01 13:34:30 +0000

    - implemented class TopHeight to support the creation of stacked bar charts - removed reference to polder in lizard_waterbalance/tests.py: as a Polder does   not exist anymore, the automatic build failed - adapted the base fixture to support the changes made by Jack to   WorkspaceItemAdapterShapefile

--- lizard_waterbalance/tests.py
+++ lizard_waterbalance/tests.py
@@ -2,18 +2,3 @@
 
 from django.test import TestCase
 
-from lizard_waterbalance.models import Polder
-
-class PolderTestSuite(TestCase):
-
-    def test_a(self):
-        """We can construct a polder."""
-        polder = Polder()
-        # If I get to here, the previous statement resulted in a new polder
-        # and that was what I wanted to know.
-
-        # If we do not use the new polder, pyflakes will file a complaint about
-        # complain about that. So to keep pyflakes happy, we test that the
-        # is not None
-        self.assertTrue(polder != None)
-
