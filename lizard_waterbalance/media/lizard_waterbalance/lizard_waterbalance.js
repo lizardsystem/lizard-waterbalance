d4b127c2965d338d5232eb38d77e8ddffaa0decc
reinout <reinout@vanrees.org>
2011-02-22 20:41:49 +0000

    Added ajax behaviour to the recalculate-button including some visual feedback.

--- lizard_waterbalance/media/lizard_waterbalance/lizard_waterbalance.js
+++ lizard_waterbalance/media/lizard_waterbalance/lizard_waterbalance.js
@@ -5,7 +5,6 @@ show_popup, nothingFoundPopup */
 
 
 function graph_type_select(event) {
-    // Reload graphs
     var $form, url;
     event.preventDefault();
     $form = $(this).parents("#graphtype-select-form");
@@ -22,6 +21,28 @@ function graph_type_select(event) {
                 div.append($("<a/>").addClass('replace-with-image').attr('href', val));
                 $('div#evenly-spaced-vertical').append(div);
             });
+            restretchExistingElements();
+        }});
+}
+
+
+function recalculate_action(event) {
+    var $form, url, $button, original_text;
+    event.preventDefault();
+    $form = $("#recalculate-form");
+    $button = $("#recalculate-submit");
+    url = $form.attr("action");
+    original_text = $button.attr("value");
+    $button.attr("value", "Aan het berekenen...");
+    $button.attr("disabled", "true");
+    $button.effect("pulsate", {"times": 2}, 2000);
+    $.ajax({
+        url: url,
+        data: $form.serialize(),
+        type: "POST",
+        success: function(data, textStatus, xhr) {
+            $button.attr("value", original_text);
+            $button.removeAttr("disabled");
             restretchExistingElements();
         }});
 }
@@ -50,5 +71,6 @@ function waterbalance_area_click_handler(x, y, map) {
 
 $(document).ready(function() {
     $("input#graph-type-select-submit").click(graph_type_select);
+    $("input#recalculate-submit").click(recalculate_action);
 });
 
