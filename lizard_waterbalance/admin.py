ed690e2cb95bc681b0f9189217143edb97e241bf
jackha <jackha@gmail.com>
2011-03-22 12:38:21 +0000

    added Parameters, pylint

--- lizard_waterbalance/admin.py
+++ lizard_waterbalance/admin.py
@@ -7,6 +7,7 @@ from lizard_waterbalance.forms import WaterbalanceTimeserieForm
 from lizard_waterbalance.models import Bucket
 from lizard_waterbalance.models import Concentration
 from lizard_waterbalance.models import OpenWater
+from lizard_waterbalance.models import Parameter
 from lizard_waterbalance.models import PumpLine
 from lizard_waterbalance.models import PumpingStation
 from lizard_waterbalance.models import Timeseries
@@ -36,18 +37,22 @@ class PumpLineInLine(admin.TabularInline):
 
 class PumpingStationAdmin(admin.ModelAdmin):
     list_filter = ('open_water', 'into', 'computed_level_control' )
-    list_display = ('name', 'open_water', 'into', 'computed_level_control', 'percentage', 'max_discharge')
+    list_display = ('name', 'open_water', 'into',
+                    'computed_level_control', 'percentage', 'max_discharge')
     search_fields = ['name', 'open_water', ]
-    
+
     inlines = [
         PumpLineInLine,
         ]
     form = PumpingStationForm
 
+
 class WaterbalanceConfAdmin(admin.ModelAdmin):
     list_filter = ('waterbalance_scenario', 'waterbalance_area',  )
-    list_display = ( 'slug', 'waterbalance_scenario', 'waterbalance_area', 'description')
+    list_display = ('slug', 'waterbalance_scenario', 'waterbalance_area',
+                    'description')
     search_fields = ['waterbalance_scenario', 'waterbalance_area' ]
+
 
 class TimeseriesEventInline(admin.TabularInline):
     model = TimeseriesEvent
@@ -62,15 +67,17 @@ class TimeseriesAdmin(admin.ModelAdmin):
 class TimeseriesFewsAdmin(admin.ModelAdmin):
     # raw_id_fields = ("fews_location",)
     form = TimeseriesFewsForm
-   
+
 
 class WaterbalanceTimeserieAdmin(admin.ModelAdmin):
     list_filter = ('use_fews', 'parameter',  )
-    list_display = ( 'name', 'use_fews', 'parameter', 'fews_timeseries', 'local_timeseries')
+    list_display = ( 'name', 'use_fews', 'parameter',
+                     'fews_timeseries', 'local_timeseries')
     search_fields = ['name',  ]
-    
+
     form = WaterbalanceTimeserieForm
-    
+
+
 class WaterbalanceAreaAdmin(admin.ModelAdmin):
     list_filter = ('public', )
     list_display = ( 'name', 'public', 'description', )
@@ -80,6 +87,7 @@ class WaterbalanceAreaAdmin(admin.ModelAdmin):
 admin.site.register(Bucket, BucketAdmin)
 admin.site.register(Concentration)
 admin.site.register(OpenWater)
+admin.site.register(Parameter)
 admin.site.register(PumpLine, PumpLineAdmin)
 admin.site.register(PumpingStation, PumpingStationAdmin)
 admin.site.register(Timeseries, TimeseriesAdmin)
