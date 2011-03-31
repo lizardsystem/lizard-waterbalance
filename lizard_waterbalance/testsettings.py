2ca017fa0f2da4818b38eac20b8ffbce14e34be1
swinkels <swinkels.pieter@yahoo.com>
2011-03-31 12:12:40 +0000

    - updated a comment

--- lizard_waterbalance/testsettings.py
+++ lizard_waterbalance/testsettings.py
@@ -9,6 +9,11 @@ DATABASES = {
     # settings in localsettings.py. Otherwise, if you change the settings in
     # the current file and commit them to the repository, other developers will
     # also use these settings whether they have that database or not.
+    #
+    # One of those other developers is Jenkins, our continuous integration
+    # solution. Jenkins can only run the tests of the current application when
+    # the specified database exists. When the tests cannot run, Jenkins sees
+    # that as an error.
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'HOST': '127.0.0.1',
