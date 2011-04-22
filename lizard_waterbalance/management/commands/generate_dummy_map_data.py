d2409a3f99bff558f202adfa412bbc341519cd66
reinout <reinout@vanrees.org>
2011-04-22 12:15:26 +0000

    Commented out a management command that isn't finished yet. It breaks the tests.

--- lizard_waterbalance/management/commands/generate_dummy_map_data.py
+++ lizard_waterbalance/management/commands/generate_dummy_map_data.py
@@ -1,42 +1,42 @@
+# Commented out by Reinout 2011-04-22 as it is no proper management commands and as it breaks the tests as it isn't importable.
+
+# from datetime import datetime
+# from os.path import join
+# import csv
+# import random
+
+# from django.core.management.base import BaseCommand
+
+# from lizard_waterbalance.compute import WaterbalanceComputer2
+# from lizard_waterbalance.models import WaterbalanceConf, WaterbalanceScenario, WaterbalanceArea, WaterbalanceTimeserie, Timeseries, TimeseriesEvent, Parameter
 
 
-from datetime import datetime
-from os.path import join
-import csv
-import random
 
-from django.core.management.base import BaseCommand
-
-from lizard_waterbalance.compute import WaterbalanceComputer2
-from lizard_waterbalance.models import WaterbalanceConf, WaterbalanceScenario, WaterbalanceArea, WaterbalanceTimeserie, Timeseries, TimeseriesEvent, Parameter
+# scenario, new = WaterbalanceScenario.objects.get_or_create(name='dummy')
+# parameter, new = Parameter.objects.get_or_create(name='dummy_month')
 
 
+# for area in WaterbalanceArea.objects.all():
+#     conf, new = WaterbalanceConf.objects.get_or_create(waterbalance_scenario=scenario, waterbalance_area=area)
+#     conf.slug = ("%s-%s"%(area.name[0:25], scenario.name)).replace(" ","_").lower()
 
-scenario, new = WaterbalanceScenario.objects.get_or_create(name='dummy')
-parameter, new = Parameter.objects.get_or_create(name='dummy_month')
+#     wb_timeseries, new = conf.results.get_or_create( name="%s-%s"%(conf.slug[0:40],parameter.name),
+#                                                 parameter = parameter)
+#     if wb_timeseries.local_timeseries:
+#         ts = wb_timeseries.local_timeseries
+#     else:
+#         ts, new = Timeseries.objects.get_or_create(name = wb_timeseries.name)
+#         wb_timeseries.local_timeseries = ts
 
+#     ts.save()
+#     conf.save()
+#     scenario.save()
+#     wb_timeseries.save()
+#     parameter.save()
 
-for area in WaterbalanceArea.objects.all():
-    conf, new = WaterbalanceConf.objects.get_or_create(waterbalance_scenario=scenario, waterbalance_area=area)
-    conf.slug = ("%s-%s"%(area.name[0:25], scenario.name)).replace(" ","_").lower()
+#     ts.timeseries_events.all().delete()
 
-    wb_timeseries, new = conf.results.get_or_create( name="%s-%s"%(conf.slug[0:40],parameter.name),
-                                                parameter = parameter)
-    if wb_timeseries.local_timeseries:
-        ts = wb_timeseries.local_timeseries
-    else:
-        ts, new = Timeseries.objects.get_or_create(name = wb_timeseries.name)
-        wb_timeseries.local_timeseries = ts
-
-    ts.save()
-    conf.save()
-    scenario.save()
-    wb_timeseries.save()
-    parameter.save()
-
-    ts.timeseries_events.all().delete()
-
-    rand_value = random.randrange(3,10)
-    for year in range(2005, 2010):
-        for month in range(1,12):
-           ts.timeseries_events.create(time=datetime(year,month,1), value=random.randrange(rand_value,10*rand_value))
+#     rand_value = random.randrange(3,10)
+#     for year in range(2005, 2010):
+#         for month in range(1,12):
+#            ts.timeseries_events.create(time=datetime(year,month,1), value=random.randrange(rand_value,10*rand_value))
