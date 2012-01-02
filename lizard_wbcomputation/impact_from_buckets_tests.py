d03ebe1454448c2a88f6efcd60a32a8a55bd93c3
swinkels <swinkels.pieter@yahoo.com>
2012-01-02 15:59:04 +0100

    Adds the module to test ImpactFromBuckets, #38

---
+++ lizard_wbcomputation/impact_from_buckets_tests.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# pylint: disable=C0111
+
+# The lizard_wbcomputation package implements the computational core of the
+# lizard waterbalance Django app.
+#
+# Copyright (C) 2012 Nelen & Schuurmans
+#
+# This package is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this package.  If not, see <http://www.gnu.org/licenses/>.
+
+from unittest import TestCase
+
+from impact_from_buckets import ImpactFromBuckets
+
+class ImpactFromBucketsTestSuite(TestCase):
+
+    def test_a(self):
+        """Test construction."""
+        impact = ImpactFromBuckets(None)
