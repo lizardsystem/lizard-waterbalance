77a3af8b44fd031ea11d5d314bd8f514265fb17a
swinkels <swinkels.pieter@yahoo.com>
2012-02-15 09:20:12 +0100

    Implements first steps to check the symmetry of a waterbalance

---
+++ lizard_wbcomputation/check_symmetry.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# pylint: disable=C0111
+
+# The lizard_wbcomputation package implements the computational core of the
+# lizard waterbalance Django app.
+#
+# Copyright (C) 2012 Nelen & Schuurmans
+#
+# This package is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this package.  If not, see <http://www.gnu.org/licenses/>.
+
+from optparse import OptionParser
+
+from timeseries.timeseries import TimeSeries
+
+
+class RelevantParameters(object):
+
+    def is_relevant(self, parameter):
+        return parameter in [
+            'Q',
+            'Q_COMP',
+            'delta_storage',
+            'discharge_drainage',
+            'discharge_drained',
+            'discharge_flow_off',
+            'discharge_hardened',
+            'indraft',
+            'precipitation',
+            'seepage',
+            'sluice_error',
+            ]
+
+
+class SummedTimeSeriesReader(object):
+    """Implements the retrieval of the summed time series from a file.
+
+    To retrieve all the time series from a given file, this class uses a
+    function supplied to the constructor and that is stored as an instance
+    attribute. This function returns all the time series as a dictionary that
+    maps a (location, parameter) tuple to a time series.
+
+    """
+
+    def __init__(self, get_time_series_as_dict):
+        self.get_time_series_as_dict = get_time_series_as_dict
+
+    def get(self, file_name):
+        """Returns the summed fraction time series from the given file."""
+        time_series = self._get_relevant_time_series(file_name)
+        return reduce(lambda x, y: x + y, time_series)
+
+    def _get_relevant_time_series(self, file_name):
+        relevant_time_series = []
+        for key, time_series in self.get_time_series_as_dict(file_name).items():
+            parameter = key[1]
+            if self.relevant_parameters.is_relevant(parameter):
+                relevant_time_series.append(time_series)
+        return relevant_time_series
+
+
+class Fractions(object):
+    """Implements the check whether the fraction time series from a file add up
+    to one.
+
+    To retrieve the summed fraction time series from a given file, this class
+    uses a so-called 'fraction reader' object that is passed to the constructor
+    and stored as an instance attribute. A fraction reader should have a method
+
+      def get(self, file_name)
+
+    that returns the summed fraction time series.
+
+    """
+    def __init__(self, fractions_reader):
+        self.fractions_reader = fractions_reader
+
+    def verify(self, file_name):
+        """Returns True if and only if the summed fractions from the given file
+        add up to one.
+
+        """
+        success = True
+        fraction_timeseries = self.fractions_reader.get(file_name)
+        for date, value in fraction_timeseries.get_events():
+            event_value = value[0]
+            success = event_value > 1 - 1e-6 and event_value < 1 + 1e-6
+            if not success:
+                print 'Failure', date, event_value
+                break
+        return success
+
+
+def main():
+    parser = OptionParser(usage="usage: %prog <PI XML time series file>")
+    (options, args) = parser.parse_args()
+    if len(args) == 1:
+        reader = SummedTimeSeriesReader(TimeSeries.as_dict)
+        reader.relevant_parameters = RelevantParameters()
+        fractions = Fractions(reader)
+        fractions.target_value = 0.0
+        if fractions.verify(args[0]):
+            return_code = 0
+        else:
+            return_code = 2
+    else:
+        parser.print_help()
+        return_code = 1
+    return return_code
---
+++ lizard_wbcomputation/check_symmetry_tests.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# pylint: disable=C0111
+
+# The lizard_wbcomputation package implements the computational core of the
+# lizard waterbalance Django app.
+#
+# Copyright (C) 2012 Nelen & Schuurmans
+#
+# This package is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this package.  If not, see <http://www.gnu.org/licenses/>.
+
+from unittest import TestCase
+
+from check_fractions_tests import MockTimeSeries
+from check_fractions_tests import create_time_series
+from check_symmetry import SummedTimeSeriesReader
+
+
+class MockRelevantParameters(object):
+
+    def is_relevant(self, parameter):
+        return parameter in ['discharge_flow_off', 'discharge_hardened']
+
+
+class SummedTimeSeriesReader_get_TestSuite(TestCase):
+
+    def test_a(self):
+        """Test the sum of a single time series."""
+        time_series = MockTimeSeries(
+            ('3201', 'discharge_flow_off', 1.0, 0.50, 0.75))
+        fractions_reader = SummedTimeSeriesReader(time_series.as_dict)
+        fractions_reader.relevant_parameters = MockRelevantParameters()
+        time_series = fractions_reader.get('waterbalance-graph.xml')
+        self.assertEqual(create_time_series(1.0, 0.50, 0.75), time_series)
+
+    def test_b(self):
+        """Test the sum of two time series."""
+        time_series = MockTimeSeries(
+            ('3201', 'discharge_flow_off',  1.0,  0.50,  0.75),
+            ('3201', 'discharge_hardened', -1.0, -0.50, -0.75))
+        fractions_reader = SummedTimeSeriesReader(time_series.as_dict)
+        fractions_reader.relevant_parameters = MockRelevantParameters()
+        time_series = fractions_reader.get('waterbalance-graph.xml')
+        self.assertEqual(create_time_series(0.0, 0.0, 0.0), time_series)
+
+    def test_c(self):
+        """Test the sum of three time series.
+
+        One of the time series is not a fraction time series.
+
+        """
+        time_series = MockTimeSeries(
+            ('3201',     'discharge_flow_off',  1.0,  0.50,  0.75),
+            ('3201',     'discharge_hardened', -1.0, -0.50, -0.75),
+            ('3201',     'fraction_water_undrained',       1.0, 0.50, 0.75))
+        fractions_reader = SummedTimeSeriesReader(time_series.as_dict)
+        fractions_reader.relevant_parameters = MockRelevantParameters()
+        time_series = fractions_reader.get('waterbalance-graph.xml')
+        self.assertEqual(create_time_series(0.0, 0.0, 0.0), time_series)
